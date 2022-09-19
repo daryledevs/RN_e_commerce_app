c136f6676fe9b2dec4bef17ff3463cd398f62d47
Daryle Karl De Vera <darylekarl.devera@gmail.com>
2022-09-19 22:07:40 +0800

    add logout button

--- src/components/navigation/DrawerStack.js
+++ src/components/navigation/DrawerStack.js
@@ -5,24 +5,34 @@ import Cart from '../user/pages/Cart';
 import Receipt from '../user/pages/Receipt';
 import Screen from './Screen';
 // libraries
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem, } from '@react-navigation/drawer';
+import { useNavigation } from '@react-navigation/native';
 // import { createStackNavigator } from '@react-navigation/stack';
 const Drawer = createDrawerNavigator();
 // const Stack = createStackNavigator();
 const DrawerStack = () => {
   const [hideHeader, setHideHeader] = React.useState(false);
+  const navigation = useNavigation();
+
+  function Foo(props){
+    return( 
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem label="Logout" onPress={() => navigation.navigate("Screen")} />
+     </DrawerContentScrollView>
+    )
+  }
+
   return (
-    <Drawer.Navigator screenOptions={!hideHeader && {headerShown: false}} >
+    <Drawer.Navigator screenOptions={!hideHeader && {headerShown: false}} 
+      useLegacyImplementation
+      drawerContent={(props) => <Foo {...props} />}
+    >
       <Drawer.Screen name="Home">
         {() => <UserHomeScreen setHideHeader={setHideHeader} />}
       </Drawer.Screen>
       <Drawer.Screen name="Cart" component={Cart} />
-      <Drawer.Screen name="Receipt" component={Receipt} />
-      <Drawer.Screen name="Logout" component={Screen} 
-        options={{
-          headerShown: false
-        }}
-      />
+      <Drawer.Screen name="Receipt" component={Receipt} />    
     </Drawer.Navigator>
   )
 }
